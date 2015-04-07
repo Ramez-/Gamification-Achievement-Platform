b03eb2f165d046888c4e985fa98846a349c9aa5b
Ramez- <ramezemadaiesec@gmail.com>
2015-04-07 10:55:23 +0200

    logo can be added by user

--- Game_Tracking_System/db/schema.rb
+++ Game_Tracking_System/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150405170803) do
+ActiveRecord::Schema.define(version: 20150407084831) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
@@ -20,8 +20,12 @@ ActiveRecord::Schema.define(version: 20150405170803) do
     t.string   "url"
     t.string   "logo"
     t.string   "description"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
