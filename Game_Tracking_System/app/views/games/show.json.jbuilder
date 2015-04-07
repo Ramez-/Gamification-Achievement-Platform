8fdb103ec5754518ca436f75305e9d72648ca23b
Omar Ali <omarali@Omars-MacBook-Pro.local>
2015-04-07 12:45:38 +0200

    User can login and logout

--- Game_Tracking_System/app/views/games/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @game, :id, :name, :game_id, :game_timezone, :url, :logo, :description, :created_at, :updated_at
--- Game_Tracking_System/config/routes.rb
+++ Game_Tracking_System/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :games
+  resources :products
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root 'devise/registrations#new'
 
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -52,6 +54,5 @@ Rails.application.routes.draw do
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  #     #   end
 end
