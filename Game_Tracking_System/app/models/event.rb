22b22b857e87494e50958772711ba3de6a8c9b25
Ahmed-wael <you@example.com>
2015-04-08 10:05:49 +0200

    #3 Adding more validations

--- Game_Tracking_System/app/models/event.rb
+++ Game_Tracking_System/app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ActiveRecord::Base
 	validates :name,
            presence: true,
            uniqueness: true
+     validates :event_id,
+           presence: true,
+           uniqueness: true
 end
