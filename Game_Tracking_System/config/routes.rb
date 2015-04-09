4b2a919b4a6ffb7dbc2aaef2d75f5a1abf64ddfc
Ramez- <ramezemadaiesec@gmail.com>
2015-04-09 19:48:02 +0200

    Tracker controller

--- Game_Tracking_System/config/routes.rb
+++ Game_Tracking_System/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'tracker/index'
+
   resources :games
   get '/game-id/:game_id' => 'games#index'
   # The priority is based upon order of creation: first created -> highest priority.
