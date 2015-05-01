d1a419f40fc52912f7ea92bb976bdd904f2cbfb6
Ramez- <ramezemadaiesec@gmail.com>
2015-05-01 16:09:34 +0300

    Adding folder node and using redis

--- Game_Tracking_System/app/assets/javascripts/application.js
+++ Game_Tracking_System/app/assets/javascripts/application.js
@@ -16,3 +16,13 @@
 //= require_tree .
 //= require websocket_rails/main
 
+window.app.realtime = {
+  connect : function(){
+    window.app.socket = io.connect("http://0.0.0.0:5001");
+
+    window.app.socket.on("rt-change", function(message){
+      // publish the change on the client side, the channel == the resource
+      window.app.trigger(message.resource, message);
+    });
+  }
+}
