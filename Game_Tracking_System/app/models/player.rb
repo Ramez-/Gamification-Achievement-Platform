545b1234346651473fa054792560650560ef0cb1
Ramez- <ramezemadaiesec@gmail.com>
2015-05-01 19:52:07 +0300

    #27 more documentation on player module

--- Game_Tracking_System/app/models/player.rb
+++ Game_Tracking_System/app/models/player.rb
@@ -1,5 +1,6 @@
 #joining with table game
 #creating validation as player_id must be unique and present
+#player id could represent the name of the player or his id number anything the user spacify 
 class Player < ActiveRecord::Base
 	validates :player_id,
            presence: true,
